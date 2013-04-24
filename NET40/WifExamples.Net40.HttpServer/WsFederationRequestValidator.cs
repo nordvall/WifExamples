612aed25e343c6d785ccc5ac83c02d48c3e60340
Nordvall <mattiasn@kuai.se>
2013-04-24 21:22:34 +0200

    New solution for .NET40

---
+++ NET40/WifExamples.Net40.HttpServer/WsFederationRequestValidator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Util;
+using Microsoft.IdentityModel.Protocols.WSFederation;
+
+namespace WifExamples.Net40.HttpServer
+{
+    public class WsFederationRequstValidator : RequestValidator
+    {
+        protected override bool IsValidRequestString(HttpContext context,
+                                string value,
+                                RequestValidationSource requestValidationSource,
+                                string collectionKey,
+                                out int validationFailureIndex)
+        {
+            validationFailureIndex = 0;
+            if (requestValidationSource == RequestValidationSource.Form &&
+              collectionKey.Equals(WSFederationConstants.Parameters.Result, StringComparison.Ordinal))
+            {
+                if (WSFederationMessage.CreateFromFormPost(context.Request) as SignInResponseMessage != null)
+                {
+                    return true;
+                }
+            }
+
+            return base.IsValidRequestString(context,
+                              value,
+                              requestValidationSource,
+                              collectionKey,
+                              out validationFailureIndex);
+        }
+    }
+}
---
+++ NET40/WifExamples.Net40.HttpsClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using WifExamples.Common;
+
+namespace WifExamples.Net40.HttpsClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ChannelFactory<IDemoService>("WS2007FederationHttpBinding_IDemoService");
+            IDemoService channel = factory.CreateChannel();
+
+            string whoAmIresult = channel.WhoAmI();
+            Console.WriteLine(whoAmIresult);
+
+            string restrictedMethodResult = channel.RestrictedMethod();
+            Console.WriteLine(restrictedMethodResult);
+
+            Console.ReadLine();
+        }
+    }
+}
