43960b708c07e55c7673753383cd5bbaccf07b46
nordvall <mattiasn@kuai.se>
2013-10-29 21:17:23 +0100

    Moved Authorize declaration from web.config to controller attributes

--- MVC4/WifExamples.MVC4.HttpServer/Controllers/HomeController.cs
+++ MVC4/WifExamples.MVC4.HttpServer/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 
 namespace WifExamples.MVC4.HttpServer.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         //
--- MVC5/WifExamples.MVC5.HttpServer/Controllers/HomeController.cs
+++ MVC5/WifExamples.MVC5.HttpServer/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 
 namespace WifExamples.MVC5.HttpServer.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         //
